bc04b691e677d04a909616571fd2cb8c2e28a3a2
Maxim Poletaev <zenwalker2@gmail.com>
2015-04-22 15:02:58 +0300

    Notifier fix

--- slack_birthdaybot/notifier.py
+++ slack_birthdaybot/notifier.py
@@ -10,9 +10,10 @@ today = date.today()
 
 with shelve.open(settings.DATABASE) as db:
     for user_id, user_data in db.items():
-        birthday = user_data["birthday"].replace(year=today.year)
+        if "birthday" in user_data:
+            birthday = user_data["birthday"].replace(year=today.year)
 
-        if birthday - delta == today:
-            info = slack.users.info(user_id).body
-            message = "Господа, через неделю %s отмечает День Рождения." % info["user"]["real_name"]
-            slack.chat.post_message("#" + settings.NOTIFIER_CHANNEL, message, username=settings.USERNAME)
+            if birthday - delta == today:
+                info = slack.users.info(user_id).body
+                message = "Господа, через неделю %s отмечает День Рождения." % info["user"]["real_name"]
+                slack.chat.post_message("#" + settings.NOTIFIER_CHANNEL, message, username=settings.USERNAME)
