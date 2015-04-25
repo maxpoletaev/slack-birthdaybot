7939541f3b236b36d7e93747f641674f51d79fa1
Maxim Poletaev <zenwalker2@gmail.com>
2015-04-25 17:55:46 +0300

    Commands support

--- slack_birthdaybot/slackrtm.py
+++ slack_birthdaybot/slackrtm.py
@@ -12,11 +12,16 @@ class RtmEvent:
         for key, val in data.items():
             setattr(self, key, val)
 
+        if self.type == "message":
+            self.prevent_command = False
+
 
 class SlackRtm:
     websocket = None
     connected = False
-    bindings = {}
+
+    _bindings = {}
+    _commands = {}
 
     def __init__(self, client, debug=False):
         self.client = client
@@ -50,18 +55,42 @@ class SlackRtm:
 
     def bind(self, event_type):
         def wrapper(func):
-            if event_type not in self.bindings:
-                self.bindings[event_type] = []
-            self.bindings[event_type].append(func)
+            if event_type not in self._bindings:
+                self._bindings[event_type] = []
+            self._bindings[event_type].append(func)
+            return func
         return wrapper
 
     def unbind(self, event_type, func):
-        if event_type in self.bindings:
-            for index, handler in enumerate(self.bindings[event_type]):
-                if handler == func:
-                    del self.bindings[index]
+        if event_type in self._bindings:
+            for index, handler in enumerate(self._bindings[event_type]):
+                if handler == func: del self._bindings[index]
 
-    def forever(self):
+    def command(self, command):
+        def wrapper(func):
+            self._commands[command] = func
+            return func
+        return wrapper
+
+    def handle_event(self, event):
+        if event.type in self._bindings:
+            for func in self._bindings[event.type]:
+                func(event)
+
+    def handle_command(self, event):
+        if event.text.startswith('!'):
+            args = event.text.split()
+            command = args[0][1:]
+
+            if command in self._commands:
+                handler = self._commands[command]
+                return handler(event, *args[1:])
+
+        if "default" in self._commands:
+            handler = self._commands["default"]
+            return handler(event)
+
+    def main_loop(self):
         self.connect()
 
         while self.connected:
@@ -70,9 +99,11 @@ class SlackRtm:
             if self.debug:
                 print(event.raw)
 
-            if event.type in self.bindings and event.subtype != "bot_message":
-                for func in self.bindings[event.type]:
-                    func(event)
+            if event.subtype != "bot_message":
+                self.handle_event(event)
+
+                if event.type == "message" and not event.prevent_command:
+                    self.handle_command(event)
 
             time.sleep(1)
 
